4502ac809cee9e552c1862e7b2c3e1f88148d001
ARLBR10 <71902040+ARLBR10@users.noreply.github.com>
2025-09-20 18:31:16 -0300

    feat(convex): Initial Schema
    Pretty much done, at least i hope!

---
+++ convex/schema.ts
@@ -0,0 +1,48 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  // First time doing this so we need to check it.
+  matches: defineTable({
+    teams: v.array(v.id("teams")),
+    scheduledData: v.optional(v.string()), // JS Date (Format: YYYY-MM-DDTHH:mm:ss.sssZ)
+    status: v.union(
+      v.literal("Scheduled"),
+      v.literal("Delayed"),
+      v.literal("Canceled"),
+      v.literal("Started"),
+      v.literal("Finished")
+    ),
+    scores: v.array(
+      v.object({
+        reversed: v.boolean(), // If it was canceled
+        player: v.union(v.id("players"), v.literal("N/A")),
+      })
+    ),
+    penalties: v.array(
+      v.object({
+        player: v.union(v.id("players")),
+        type: v.string() // Add some literals on the future
+      })
+    ),
+  }),
+  teams: defineTable({
+    name: v.string(),
+    sport: v.string(), // Create a LIST at the page
+    type: v.union(v.literal("Feminine"), v.literal("Masculine")),
+    players: v.array(v.id("players")),
+  }),
+  players: defineTable({
+    name: v.string(),
+    alias: v.array(v.string()),
+    height: v.optional(v.string()),
+    weight: v.optional(v.string()),
+    age: v.optional(v.number()),
+    photo: v.optional(v.string()), // URL *maybe?
+    socialMedias: v.optional(
+      v.object({
+        instagram: v.optional(v.string()), // Maybe some other?
+      })
+    ),
+  }),
+});
