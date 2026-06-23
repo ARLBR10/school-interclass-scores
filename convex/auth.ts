d109068525d2bebe9e2f33db510f9585091f28d8
ARLBR10 <71902040+ARLBR10@users.noreply.github.com>
2026-06-22 21:02:01 -0300

    refactor: Better Auth component with a custom schema
    * Rather than only the [built-in
      plugins](https://labs.convex.dev/better-auth/supported-plugins)
    * [Tutorial](https://labs.convex.dev/better-auth/features/local-install)

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,4 +1,7 @@
-import { betterAuth } from 'better-auth/minimal'
+import {
+  betterAuth,
+  type BetterAuthOptions
+} from 'better-auth/minimal'
 import { createClient } from '@convex-dev/better-auth'
 import { convex } from '@convex-dev/better-auth/plugins'
 import authConfig from './auth.config'
@@ -6,15 +9,10 @@ import { components } from './_generated/api'
 import { env, query } from './_generated/server'
 import type { GenericCtx } from '@convex-dev/better-auth'
 import type { DataModel } from './_generated/dataModel'
+import authSchema from "./betterAuth/schema"; 
 
-const siteUrl = env.SITE_URL
-
-// The component client has methods needed for integrating Convex with Better Auth,
-// as well as helper methods for general use.
-export const authComponent = createClient<DataModel>(components.betterAuth)
-
-export const createAuth = (ctx: GenericCtx<DataModel>) => {
-  return betterAuth({
+export const createAuthOptions = (ctx: GenericCtx<DataModel>) => {
+  return {
     baseURL: siteUrl,
     database: authComponent.adapter(ctx),
     // Configure simple, non-verified email/password to get started
@@ -26,7 +24,24 @@ export const createAuth = (ctx: GenericCtx<DataModel>) => {
       // The Convex plugin is required for Convex compatibility
       convex({ authConfig }),
     ],
-  })
+  } satisfies BetterAuthOptions;
+};
+
+const siteUrl = env.SITE_URL
+
+// The component client has methods needed for integrating Convex with Better Auth,
+// as well as helper methods for general use.
+export const authComponent = createClient<DataModel, typeof authSchema>( 
+  components.betterAuth,
+  {
+    local: {
+      schema: authSchema,
+    },
+  }
+);
+
+export const createAuth = (ctx: GenericCtx<DataModel>) => {
+  return betterAuth(createAuthOptions(ctx))
 }
 
 // Example function for getting the current user
--- convex/convex.config.ts
+++ convex/convex.config.ts
@@ -1,6 +1,6 @@
 import { defineApp } from 'convex/server'
 import { v } from 'convex/values'
-import betterAuth from '@convex-dev/better-auth/convex.config'
+import betterAuth from "./betterAuth/convex.config"; 
 
 const app = defineApp({
   env: {
